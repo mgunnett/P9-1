0cff56e95d5d03f20e057c8a19c4e80bdb73bba3
mgunnett <m-gunnett@onu.edu>
2024-11-06 10:19:38 -0500

    Every element at an even index.

--- P9.1/P9-1.cpp
+++ P9.1/P9-1.cpp
@@ -4,9 +4,15 @@
 
 using namespace std;
 
+void getEvenIndexElements(int arr1[], int size, int evenIndexArray[], int& evenIndexSize);
+
+
 int main() {
 
-	int arr1[10];
+	int arr1[10],
+		evenIndexArray[10],
+		evenIndexSize;
+
 	int const size = 10;
 
 	// Initialize random values for the array.
@@ -16,8 +22,25 @@ int main() {
 
 	for (int i = 0; i < size; i++) {
 		arr1[i] = 1000 + rand() % 9000;
-		cout << " " << arr1[i];
+		cout << arr1[i] << " ";
 	}
 
+	// Get values for even index array and print them
+	getEvenIndexElements(arr1, size, evenIndexArray, evenIndexSize);
 
+	cout << "\nEven Indices: ";
+
+	for (int i = 0; i < evenIndexSize; i++) {
+		cout << evenIndexArray[i] << " ";
+	}
+
+}
+
+void getEvenIndexElements(int arr1[], int size, int evenIndexArray[], int& evenIndexSize) {
+	evenIndexSize = 0;
+	for (int i = 0; i < size; i++) {
+		if (i % 2 == 0) {
+			evenIndexArray[evenIndexSize++] = arr1[i];
+		}
+	}
 }
